85a790d1eb36607551d59f85de267f5f91da3c23
Carolina Daniel <arq.carolinadaniel@gmail.com>
2023-07-04 15:55:28 -0300

    [Update] ex03

--- cpp_module_01/ex03/Weapon.hpp
+++ cpp_module_01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 11:51:04 by cado-car          #+#    #+#             */
-/*   Updated: 2023/07/01 13:20:48 by cado-car         ###   ########.fr       */
+/*   Updated: 2023/07/01 14:47:27 by cado-car         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define WEAPON_HPP
 
 # include <string>
+
+// Color codes
+const std::string RESET = "\033[0m";
+const std::string RED = "\033[31m";
+const std::string GREEN = "\033[32m";
+const std::string YELLOW = "\033[33m";
 
 class Weapon {
 private:
--- cpp_module_01/ex03/main.cpp
+++ cpp_module_01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 13:17:29 by cado-car          #+#    #+#             */
-/*   Updated: 2023/07/01 13:18:20 by cado-car         ###   ########.fr       */
+/*   Updated: 2023/07/01 14:49:03 by cado-car         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 int main()
 {
+	std::cout << std::endl;
+	std::cout << "Attack from HumanA" << std::endl;
+	std::cout << "------------------" << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << YELLOW;
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanA bob("Bob", club);
@@ -22,6 +28,14 @@ int main()
 		club.setType("some other type of club");
 		bob.attack();
 	}
+	std::cout << RESET;
+
+	std::cout << std::endl;
+	std::cout << "Attack from HumanB" << std::endl;
+	std::cout << "------------------" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << YELLOW;
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
@@ -30,5 +44,6 @@ int main()
 		club.setType("some other type of club");
 		jim.attack();
 	}
+	std::cout << RESET;
 	return (0);
 }
